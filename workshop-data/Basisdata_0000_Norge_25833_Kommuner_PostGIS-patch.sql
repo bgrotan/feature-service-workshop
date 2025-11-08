6f096674828063d87b09c3863832507fbf229067
Bjørn Ove Grøtan <bjorn.ove.grotan@vegvesen.no>
2025-11-08 14:09:45 +0100

    Add primary keys for tables

--- workshop-data/Basisdata_0000_Norge_25833_Kommuner_PostGIS-patch.sql
+++ workshop-data/Basisdata_0000_Norge_25833_Kommuner_PostGIS-patch.sql
@@ -1,1 +1,5 @@
+alter table kommuner_95b1247e0400454f971d957671dc3744.administrativenhetnavn add primary key (objid);
+alter table kommuner_95b1247e0400454f971d957671dc3744.grense add primary key (objid);
 alter table kommuner_95b1247e0400454f971d957671dc3744.kommune add primary key (objid);
+alter table kommuner_95b1247e0400454f971d957671dc3744.sprakkode add primary key (identifier);
+alter table kommuner_95b1247e0400454f971d957671dc3744.typeavgrensning add primary key (identifier);
